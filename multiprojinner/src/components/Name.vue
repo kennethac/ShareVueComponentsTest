ea68e01505e5a46d0d06e2804f5a060872717bbe
Kenneth Christensen <1kennethac@gmail.com>
2019-09-05 08:22:52 -0600

    Demonstration commit.

---
+++ multiprojinner/src/components/Name.vue
@@ -0,0 +1,14 @@
+<template>
+  <p>Hello {{ name }}</p>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Name extends Vue {
+  get name() {
+    return this.$store.state.name;
+  }
+}
+</script>
---
+++ multiprojinner/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <Name />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import Name from '@/components/Name.vue' // @ is an alias to /src
+
+@Component({
+  components: {
+    Name
+  }
+})
+export default class Home extends Vue {}
+</script>
